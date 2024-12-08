a60c8c7213b2ed4b7c9604378b6cfb9449a4ab92
Ishwar Singh Bhandari <78526992+IshwarSinghBhandari@users.noreply.github.com>
2024-12-08 12:34:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,25 +6,24 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Initialize dotenv for environment variables
 dotenv.config();
 
-// Set up __dirname in ES Modules
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Database Connection
+
 dbConnect()
     .then(() => console.log('Database connected'))
     .catch((err) => console.error('Database connection error:', err));
 
-// Initialize Express app
+
 const app = express();
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
-app.set('view engine', 'ejs'); // Set view engine to EJS
+app.use(express.static(path.join(__dirname, 'public'))); 
+app.set('view engine', 'ejs');
 
 // Routes
 app.use("/", allroute);
